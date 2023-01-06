f0c86aaab739aadad6a741c14224356a395c1e54
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-06 14:38:21 -0700

    comment cleanup

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,9 +6,6 @@ type UserOnRequest = Omit<User, 'password'>;
 declare global {
 	namespace Express {
 		export interface Request {
-			/**
-			 * This will only be populated if the corresponding middleware populates these values first
-			 */
 			user: UserOnRequest;
 		}
 	}
