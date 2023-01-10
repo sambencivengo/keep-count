f2ea3eb321b79d7f671ef74c3f191eb4018036ec
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-09 17:15:21 -0700

    increment and decrement is working!

--- src/api/counts/[countId]/put.ts
+++ src/api/counts/[countId]/put.ts
@@ -4,11 +4,32 @@ import { prisma } from '../../../prismaClient';
 export const put: Handler = async (req, res) => {
 	const { countId } = req.params;
 	const { user } = req;
-	const count = await prisma.count.findUnique({
+	const { purpose } = req.query;
+
+	const count = await prisma.user.update({
 		where: {
-			id: Number(countId),
+			id: user.id,
+		},
+		data: {
+			counts: {
+				update: {
+					where: {
+						id: Number(countId),
+					},
+					data: {
+						value:
+							purpose === 'add'
+								? {
+										increment: 1,
+								  }
+								: {
+										decrement: 1,
+								  },
+					},
+				},
+			},
 		},
 	});
 
-	res.send({ countId, user });
+	res.send({ count, user });
 };
