3bc45604dc9d995a716e92a905ec6326707ff801
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-11 08:29:04 -0700

    ripping out client

--- src/index.ts
+++ src/index.ts
@@ -7,50 +7,60 @@ import { api } from './api';
 import createMemoryStore from 'memorystore';
 import session from 'express-session';
 import cors from 'cors';
+import next from 'next';
 
 const MemoryStore = createMemoryStore(session);
 const PORT = process.env.PORT ?? 8000;
+const dev = process.env.nodeEnv !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const start = async () => {
-	const app = express();
+// const start = async () => {
 
-	await connectDb();
+app.prepare()
+	.then(async () => {
+		const server = express();
 
-	app.use(
-		cors<cors.CorsRequest>({
-			origin: ['http://localhost:3000'],
-			credentials: true,
-		})
-	);
+		await connectDb();
 
-	app.use(
-		session({
-			store: new MemoryStore({
-				checkPeriod: 86400000,
-			}),
-			name: 'keep-count-cookie',
-			cookie: {
-				maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-				httpOnly: false,
-				sameSite: 'lax', // csrf
-				secure: false, // TODO: switch to constant
-			},
-			saveUninitialized: false,
-			secret: 'cookiesecret', // TODO: switch to env var
-			resave: true,
-		})
-	);
+		server.use(
+			cors<cors.CorsRequest>({
+				origin: ['http://localhost:3000'],
+				credentials: true,
+			})
+		);
 
-	app.use(express.json());
+		server.use(
+			session({
+				store: new MemoryStore({
+					checkPeriod: 86400000,
+				}),
+				name: 'keep-count-cookie',
+				cookie: {
+					maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+					httpOnly: false,
+					sameSite: 'lax', // csrf
+					secure: false, // TODO: switch to constant
+				},
+				saveUninitialized: false,
+				secret: 'cookiesecret', // TODO: switch to env var
+				resave: true,
+			})
+		);
 
-	// All api routes
-	app.use('/api', api);
+		server.use(express.json());
 
-	app.listen(PORT, () => {
-		console.log(`Application is listening on port ${PORT}`);
-	});
-};
-start()
+		// All api routes
+		server.use('/api', api);
+
+		server.get('/*', (req, res) => {
+			return handle(req, res);
+		});
+
+		server.listen(PORT, () => {
+			console.log(`ApplicatIon is listening on port ${PORT}`);
+		});
+	})
 	.catch((e) => {
 		console.error(e);
 		process.exit(1);
@@ -58,3 +68,6 @@ start()
 	.finally(async () => {
 		await prisma.$disconnect();
 	});
+
+// };
+// start()
